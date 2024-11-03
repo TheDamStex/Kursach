98dad7cf884225f54397260cec8b790a3b566e51
TheDamStex <119650384+TheDamStex@users.noreply.github.com>
2024-11-04 00:14:59 +0200

    04.11

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Kursach
 {
@@ -24,5 +11,34 @@ namespace Kursach
         {
             InitializeComponent();
         }
+
+        // Открытие окна с расписанием автобусов
+        private void OpenScheduleWindow(object sender, RoutedEventArgs e)
+        {
+            ScheduleWindow scheduleWindow = new ScheduleWindow();
+            scheduleWindow.Show();
+        }
+
+        // Открытие окна оформления билетов
+         private void OpenTicketWindow(object sender, RoutedEventArgs e)
+         {
+         TicketWindow ticketWindow = new TicketWindow();
+         ticketWindow.Show();
+         }
+
+        // Открытие окна авторизации
+        private void OpenLoginWindow(object sender, RoutedEventArgs e)
+        {
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.Show();
+        }
+
+        // Открытие окна регистрации
+        private void OpenRegistrationWindow(object sender, RoutedEventArgs e)
+        {
+            RegistrationWindow registrationWindow = new RegistrationWindow();
+            registrationWindow.Show();
+        }
     }
 }
+
--- RegistrationWindow.xaml.cs
+++ RegistrationWindow.xaml.cs
@@ -1,27 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using System;
+using System.IO;
+using System.Text.Json;
 
 namespace Kursach
 {
-    /// <summary>
-    /// Логика взаимодействия для RegistrationWindow.xaml
-    /// </summary>
     public partial class RegistrationWindow : Window
     {
         public RegistrationWindow()
         {
             InitializeComponent();
         }
+
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Получение данных из текстовых полей
+            string login = LoginTextBox.Text;
+            string password = PasswordBox1.Password;
+            string confirmPassword = PasswordBox2.Password;
+
+            // Проверка совпадения паролей
+            if (password != confirmPassword)
+            {
+                MessageBox.Show("Пароли не совпадают!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            // Создание объекта пользователя
+            var user = new User
+            {
+                Login = login,
+                Password = password
+            };
+
+            // Сериализация в JSON
+            string json = JsonSerializer.Serialize(user);
+
+            // Сохранение JSON в файл
+            string filePath = "user_data.json";
+            File.WriteAllText(filePath, json);
+
+            MessageBox.Show("Регистрация успешна!", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+
+    // Модель пользователя
+    public class User
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
     }
 }
