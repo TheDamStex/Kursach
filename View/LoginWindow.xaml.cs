dab35b6332d4f21b9d2b1203595db3f4bdaa2c71
annavarych <annavarychh@gmail.com>
2024-11-24 21:32:40 +0200

    24.11.24

--- View/LoginWindow.xaml.cs
+++ View/LoginWindow.xaml.cs
@@ -20,5 +20,31 @@ namespace Kursach.View
                 viewModel.Password = (sender as PasswordBox)?.Password;
             }
         }
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            LoginHint.Visibility = Visibility.Collapsed; // Скрыть подсказку, когда поле активно
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(LoginTextBox.Text))
+            {
+                LoginHint.Visibility = Visibility.Visible; // Показать подсказку, если поле пустое
+            }
+        }
+
+        // Подсказки для пароля
+        private void PasswordBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            PasswordHint.Visibility = Visibility.Collapsed; // Скрыть подсказку, когда поле активно
+        }
+
+        private void PasswordBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(PasswordBox.Password))
+            {
+                PasswordHint.Visibility = Visibility.Visible; // Показать подсказку, если поле пустое
+            }
+        }
     }
 }
--- View/RegistrationWindow.xaml.cs
+++ View/RegistrationWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 using Kursach.ViewModel;
 
 namespace Kursach.View
@@ -24,6 +25,38 @@ namespace Kursach.View
             if (DataContext is RegistrationViewModel viewModel)
             {
                 viewModel.ConfirmPassword = PasswordBox2.Password;
+            }
+        }
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            LoginHint.Visibility = Visibility.Collapsed; // Скрыть подсказку, когда поле активно
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty((sender as TextBox)?.Text))
+            {
+                LoginHint.Visibility = Visibility.Visible; // Показать подсказку, если поле пустое
+            }
+        }
+
+        // Подсказки для пароля
+        private void PasswordBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            PasswordHint1.Visibility = Visibility.Collapsed; // Скрыть подсказку, когда поле активно
+            PasswordHint2.Visibility = Visibility.Collapsed;
+        }
+
+        private void PasswordBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(PasswordBox1.Password))
+            {
+                PasswordHint1.Visibility = Visibility.Visible; // Показать подсказку, если поле пустое
+            }
+
+            if (string.IsNullOrEmpty(PasswordBox2.Password))
+            {
+                PasswordHint2.Visibility = Visibility.Visible;
             }
         }
     }
