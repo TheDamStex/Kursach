8dbd4a765d38c5e39eefecd3f4b857195071d822
annavarych <annavarychh@gmail.com>
2024-11-28 16:14:48 +0200

    28.11.24

--- View/PaymentWindow.xaml.cs
+++ View/PaymentWindow.xaml.cs
@@ -3,6 +3,7 @@ using Kursach.ViewModel;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace Kursach.View
 {
@@ -10,7 +11,9 @@ namespace Kursach.View
     {
         public PaymentWindow()
         {
+            
             InitializeComponent();
+            
         }
         
 
@@ -21,5 +24,88 @@ namespace Kursach.View
                 viewModel.CVV = ((PasswordBox)sender).Password;
             }
         }
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null)
+            {
+                // Скрываем водяной знак, если в поле уже есть текст
+                TextBlock watermark = FindChild<TextBlock>(textBox, "WatermarkTextBlock");
+                if (watermark != null && string.IsNullOrEmpty(textBox.Text))
+                {
+                    watermark.Visibility = Visibility.Collapsed;
+                }
+            }
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null)
+            {
+                // Показываем водяной знак, если поле пустое
+                TextBlock watermark = FindChild<TextBlock>(textBox, "WatermarkTextBlock");
+                if (watermark != null && string.IsNullOrEmpty(textBox.Text))
+                {
+                    watermark.Visibility = Visibility.Visible;
+                }
+            }
+        }
+
+        private void PasswordBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            PasswordBox passwordBox = sender as PasswordBox;
+            if (passwordBox != null)
+            {
+                // Скрываем подсказку
+                TextBlock watermark = FindChild<TextBlock>(passwordBox, "WatermarkTextBlock");
+                if (watermark != null)
+                {
+                    watermark.Visibility = Visibility.Collapsed;
+                }
+            }
+        }
+
+        private void PasswordBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            PasswordBox passwordBox = sender as PasswordBox;
+            if (passwordBox != null)
+            {
+                // Показываем подсказку, если поле пустое
+                TextBlock watermark = FindChild<TextBlock>(passwordBox, "WatermarkTextBlock");
+                if (watermark != null && string.IsNullOrEmpty(passwordBox.Password))
+                {
+                    watermark.Visibility = Visibility.Visible;
+                }
+            }
+        }
+
+        private T FindChild<T>(DependencyObject parent, string childName) where T : DependencyObject
+        {
+            int childrenCount = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < childrenCount; i++)
+            {
+                var child = VisualTreeHelper.GetChild(parent, i);
+                if (child is T)
+                {
+                    T childOfType = (T)child;
+                    if (childOfType is FrameworkElement fe && fe.Name == childName)
+                    {
+                        return childOfType;
+                    }
+                }
+
+                // Рекурсивно ищем в дочерних элементах
+                T result = FindChild<T>(child, childName);
+                if (result != null)
+                {
+                    return result;
+                }
+            }
+
+            return null;
+        }
+
     }
+
 }
